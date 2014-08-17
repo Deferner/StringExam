e75a2fac816b5a72f9d5580417d736b6320f1d79
Deferner <deferner-on@hotmail.com>
2014-08-17 01:09:50 -0700

    create new java code

---
+++ src/SimpleInheritance.java
@@ -0,0 +1,37 @@
+
+public class SimpleInheritance {
+	public static void main(String [] args){
+		MemCustomer cus = new MemCustomer();
+		System.out.println("Customer ID : " +cus.getCusId());
+		System.out.println("Customer name : " +cus.getCusName());
+		System.out.println("Sum production price : " +cus.getTotalPrice());
+		System.out.println("Discount : " +cus.get.Discount());
+		cus.calNetPrice();
+	}
+}
+class Customer{
+	String cusId = "A0101";
+	String cusName = "Deferner Breaker";
+	double totalPrice = 2532, netPrice = 0.0;
+	
+	public String getCusId(){
+		return cusId;
+	}
+	public String getCusName(){
+		return cusName;
+	}
+	public Double getTotalPrice(){
+		return totalPrice;
+	}
+}
+class MemCustomer extends Customer{
+	double discount;
+	
+	public double getDiscount(){
+		return discount = 0.15;
+	}
+	public void calNetPrice(){
+		netPrice = totalPrice - (totalPrice*discount);
+		System.out.println("Price after discount : " +netPrice);
+	}
+}
